1eadc7f12b83ae6ce75fd8584418c792ac01d850
root <root@ip-172-31-8-158.ap-south-1.compute.internal>
2022-02-08 16:56:56 +0000

    443 port opened for new app

--- main.tf
+++ main.tf
@@ -18,6 +18,17 @@ resource "aws_security_group" "webserver" {
     ipv6_cidr_blocks = [ "::/0" ]
   }
 
+#edited by jijuTomL
+
+  ingress {
+    description      = ""
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
+  }
+
     
   egress {
     from_port        = 0
